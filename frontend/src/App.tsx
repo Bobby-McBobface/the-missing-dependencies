9c52316924c6263ee2623412f67ea91ad66affa9
Gagan <rollingrocky360@gmail.com>
2023-09-10 01:29:41 +0530

    Add: UI buttons for proposed functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import Navbar from "./components/Navbar";
 import Output from "./components/Output";
 import Editor from "./components/Editor";
@@ -6,18 +5,12 @@ import Editor from "./components/Editor";
 import './App.css'
 
 const App = () => {
-    const [output, setOutput] = useState('');
-
-    const handleCodeExecution = (outputText) => {
-        setOutput(outputText);
-        console.log(outputText);
-    };
 
     return (
         <div id="app">
             <Navbar />
-            <Editor onCodeExecution={handleCodeExecution} />
-            <Output output={output} />
+            <Editor />
+            <Output />
         </div>
     );
 };
